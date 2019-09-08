271ad88d0c919b3117b34ccce86cbca69f3492e2
bghbmh <55042013+bghbmh@users.noreply.github.com>
2019-09-08 14:30:23 +0900

    Add files via upload

---
+++ data/save.php
@@ -0,0 +1,68 @@
+<?php
+  // create short variable names
+  
+  $file_name = $_POST['file_name'];
+  
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<title> data save/load  </title>
+</head>
+<body>
+
+<?php
+ 
+
+  if($file_name == "index_html")
+  {
+	$agree = $_POST['agree'];
+	$whatDay = $_POST['whatDay'];
+	$name = $_POST['name'];
+	$tel = $_POST['tel'];
+	$story = $_POST['story'];	
+	
+	 echo $whatDay;
+
+    $outputstring =  $whatDay.",".$name.",".$tel.",".$story.",".$agree."\n";
+
+    $fp = fopen("askMe.txt", 'a');
+
+    flock($fp, LOCK_EX);
+    if (!$fp) {
+      echo "no";
+      exit;
+    }
+    
+    fwrite($fp, $outputstring, strlen($outputstring));
+    flock($fp, LOCK_UN);
+    fclose($fp);
+
+    header("location:../index.html");
+  }
+  else if($file_name == "admin_php")
+  {
+    $allData = $_POST['reData'];
+
+    echo $allData;
+
+    $fileopen = @fopen("askMe.txt",'w') or exit("파일을 읽을 수 없습니다."); 
+
+    flock($fileopen, LOCK_EX);
+    if (!$fileopen) {
+      echo "no";
+      exit;
+    }
+    
+    fwrite($fileopen, $allData);
+    flock($fileopen, LOCK_UN);
+    fclose($fileopen);
+    
+    header("location: admin.php");
+  }
+
+?>
+
+</body>
+</html>
