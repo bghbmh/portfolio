b60a410ce1a7c4a890906c48970f86826ce20b45
bghbmh <55042013+bghbmh@users.noreply.github.com>
2019-09-20 12:00:08 +0900

    a

--- js/bmh.js
+++ js/bmh.js
@@ -1,6 +1,6 @@
 	var agree = 0;
-	var w = window.innerWidth;	
-	var h = window.innerHeight;	
+	var w = window.innerWidth;
+	var h = window.innerHeight;
 	var pw = 0;
 	var ph = 0;
 	var d = new Date();
@@ -10,35 +10,35 @@
 		//alert(w);
 		var ww = window.innerWidth;
 		var wh = window.innerHeight;
-			
+
 		var ctn = document.getElementById("contents");
 		var area = ctn.getElementsByTagName("div");
 		var imgH = [];
 		imgH = document.getElementsByClassName("pi");
-			
+
 		var n=0;
 		for( n=0; n<area.length; n++)
 		{ //alert( imgH[n].src);
-			if( imgH[n].height >= wh )				
+			if( imgH[n].height >= wh )
 				area[n].style.height = imgH[n].height/40 + "rem";
 		}
-		
+
 		/*
-		do{ alert(x[n].style.height +'/'+x[n].height); 
-			if( x[n].height >= wh )				
+		do{ alert(x[n].style.height +'/'+x[n].height);
+			if( x[n].height >= wh )
 				area[n].style.height = x[n].height/40 + "rem";
-			
-			
+
+
 			n++;
-		}	
+		}
 		while ( area[n].style.display != "none");
 */
 	}
 
-	
+
 	function telNum(){
 		var bar= document.getElementById("talk");
-		
+
 		if( bar.getElementsByTagName("a")[0].style.display == "inline-block" )
 		{
 			bar.style.width = "6rem";
@@ -46,7 +46,7 @@
 		}
 		else
 		{
-			bar.style.width = "20rem";			
+			bar.style.width = "20rem";
 			bar.getElementsByTagName("a")[0].style.display = "inline-block";
 		}
 	}
@@ -54,13 +54,13 @@
 
 
 
-	
-	
+
+
 	/*==========================================================================================*/
-	
-	
-	function introduce(className, num){  
-		
+
+
+	function introduce(className, num){
+
 		openPop(className, num);
 
 		var popObject = document.getElementsByClassName(className)[num];
@@ -74,26 +74,26 @@
 		{//alert("!");
 			document.getElementById("popBg").style.display ="block";
 		}
-		
+
 	}
-	
-	function bigShow(obj){ 
-		var w = window.innerWidth;	
-		var h = window.innerHeight;	
+
+	function bigShow(obj){
+		var w = window.innerWidth;
+		var h = window.innerHeight;
 		var show = document.getElementById("imgBig");
-		
+
 		document.getElementById("popBg").style.display = "block";
 		document.getElementById("watchingArea").style.display = "block";
 		document.getElementById("watchingArea").style.height = h +"px";
 		document.getElementById("watchingArea").scrollTop = 0;
 
-		show.src = obj.src;	
-		
+		show.src = obj.src;
+
 		var ow = 0;
 		var oh = 0;
 		ow = show.clientWidth;
 		oh = show.clientHeight;
-		
+
 		if( oh == 0 )
 		{ //alert('oh == 0'+oh + '/' +obj.height);
 			oh = obj.height;
@@ -110,25 +110,25 @@
 				show.clientWidth = ow + "px";
 				//alert(w +'/'+ show.style.width);
 			}
-			
+
 			show.style.top = (h - oh)/2 +"px";
 		}
 		else
-		{ 
+		{
 			show.style.top = 20 +"px";
-			
+
 		}
-		
-		show.style.marginLeft = (w - ow)/2+ "px";			
-	}	
-	
-	
+
+		show.style.left = (w - ow)/2+ "px";
+	}
+
+
 	function ask(n){
 		agree = agree + n;
 
 		if( agree%2 == 1 )
 		{
-			document.getElementsByClassName("CheckBox")[0].style.backgroundColor = "#4CAF50";	
+			document.getElementsByClassName("CheckBox")[0].style.backgroundColor = "#4CAF50";
 			document.forms["question"]["agree"].value = "yes";	 // alert(document.forms["question"]["agree"].value);
 		}
 		else if( agree%2 == 0 )
@@ -137,38 +137,38 @@
 			document.forms["question"]["agree"].value = "no";
 		}
 	}
-	
-	
-	function openPop(className, num){ 
-		var w = window.innerWidth;	
-		var h = window.innerHeight;	
+
+
+	function openPop(className, num){
+		var w = window.innerWidth;
+		var h = window.innerHeight;
 		var popObject = document.getElementsByClassName(className)[num];
-		
+
 		popObject.style.display = "block";
-		
+
 		pw =  popObject.clientWidth;
-		ph =  popObject.clientHeight; 
+		ph =  popObject.clientHeight;
 
 		//alert('openPop' + pw+'/'+ph);
 
 		popObject.style.left = (w-pw)/2 + "px";
-		popObject.style.top = (h-ph)/2 + "px";		
+		popObject.style.top = (h-ph)/2 + "px";
 	}
-	
-	
-	
-	function closePop(className, num, str){		
+
+
+
+	function closePop(className, num, str){
 		//alert(className);
 		if ( str == "alarm" )
-		{			
-			if( fc != "done" ) 
+		{
+			if( fc != "done" )
 			{
 				document.getElementById("popBg").style.zIndex = "555";
 				if( fc != "agree" ) document.getElementById(fc).focus();
 			}
-			
+
 			document.getElementsByClassName(className)[num].style.display = "none";
-								
+
 		}
 		else if( str == "call_off")
 		{
@@ -193,34 +193,34 @@
 			     document.getElementsByClassName(className)[num].style.display = "none";
                         }
 
-			if( document.getElementById("popBg").style.display != "none" )			
+			if( document.getElementById("popBg").style.display != "none" )
 				document.getElementById("popBg").style.display = "none";
-			
-	
-				
+
+
+
 		}
 		else if( str == "go" )
 		{  			//alert("go"+className+'/'+str);
-			
+
 			document.getElementsByClassName(className)[num].style.display = "none";
 			document.getElementsByClassName("Hello")[1].style.display = "none";
-			document.getElementById("popBg").style.display = "none";	
-			//alert(story);	
+			document.getElementById("popBg").style.display = "none";
+			//alert(story);
 			story = document.forms["question"]["story"].value;
-			story = story.replace(/\n/gi, " ");	
+			story = story.replace(/\n/gi, " ");
 			document.forms["question"]["story"].value = story;
-			
-			document.forms["question"]["whatDay"].value = d.getFullYear() + "년" + ( d.getMonth() + 1) +"월" +d.getDate() +"일 \t" + d.getHours() +"시"+ d.getMinutes() +"분";				
-			//alert(story);	
+
+			document.forms["question"]["whatDay"].value = d.getFullYear() + "년" + ( d.getMonth() + 1) +"월" +d.getDate() +"일 \t" + d.getHours() +"시"+ d.getMinutes() +"분";
+			//alert(story);
 			//document.getElementById("ok").method = "post";
 			document.getElementById("ok").action = "http://bghbmh.dothome.co.kr/data/save.php";
-			document.getElementById("ok").submit();	
+			document.getElementById("ok").submit();
 		}
 		/*
 		*/
 
 	}
-	
+
 	function checking()
 	{
 		var btn = "확인";
@@ -229,15 +229,15 @@
 		var strStory="문의사항을 입력해주세요";
 		var strAgree="개인정보 수집 및 이용에 동의해주세요";
 		var strOk = "접수되었습니다<br>최종확인을 위해 빠른 시간 안에<br>연락드리겠습니다";
-		
+
 		var name = document.forms["question"]["name"].value;
 		var tel = document.forms["question"]["tel"].value;
 		var story = document.forms["question"]["story"].value;
-		
+
 		document.getElementById("popBg").style.zIndex = "8888";
-		if (name == "") 
-		{ 
-			fc = "name";			
+		if (name == "")
+		{
+			fc = "name";
 			openAlert(1, strName, btn);
 		}
 		else if(tel == "")
@@ -256,36 +256,34 @@
 			openAlert(1, strAgree, btn);
 		}
 		else
-		{			
+		{
 			openAlert(2, strOk, btn);
 		}
 	}
-	
-	
+
+
 	function openAlert(n, str1, str2){   //alert(str1);
-	
+
 		var popWarnning = document.getElementsByClassName("popWarnning")[0];
 		var popTxt = document.getElementsByClassName("popTxt")[1];
 		var popBtn = document.getElementsByClassName("popBtn")[n];
 
 		popWarnning.style.display = "block";
 		popBtn.style.display = "block";
-		
+
 		if(n==2) document.getElementsByClassName("popBtn")[n-1].style.display = "none";
 		//document.getElementsByClassName("popBtn")[1].style.display = "none";
-		
+
 		popTxt.innerHTML = str1;
 		popBtn.innerHTML = str2;
 
 		var popw = popWarnning.clientWidth;
 		var poph = popWarnning.clientHeight;
 		//alert(poph);
-		
+
 		popWarnning.style.top = (h-poph)/2 + "px";
 		popWarnning.style.left = (w-popw)/2 + "px";
-		
+
 		return;
-		
-	} 	
-	
-	
+
+	}
