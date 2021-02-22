10e220bbcbf62ac2322cedbd57391c1b606dc45c
bghbmh <55042013+bghbmh@users.noreply.github.com>
2021-02-22 15:31:40 +0900

    Update popup.js

--- test/js/popup.js
+++ test/js/popup.js
@@ -4,170 +4,155 @@
 'use strict';
 
 
-let timeCheck; 
 
-function openPopup(obj, mainbody)
+
+function openPopup(e) //obj, mainbody
 {
-	console.log(`obj.className :  ${obj.className}, mainbody.classname : ${mainbody.className} ` );
 
-	set_popup(true); // black bg 깔고
+	let pp = "." + e.currentTarget.getAttribute('class') + 'popup';
 
-	timeCheck = -1; 
+	const popup = document.querySelector(pp);
+	console.log(popup);
+
+	set_popup(); // black bg 깔고
 
 	const popX = check_element_positionX();
 	const scroll_top_holder = check_element_positionY(); //스크롤 위치 확인
 
-	obj.style.display = "block";  //팝업 열고
+	popup.style.display = "block";  //팝업 열고
+	// timeCheck = -1; 
+	set_input(popup);
 
-	obj.style.top = (window.innerHeight - obj.clientHeight)/2 + "px"; // 팝업 위치 정하고
-	obj.style.left = (window.innerWidth - obj.clientWidth)/2 + "px";
+	popup.style.top = (window.innerHeight - popup.clientHeight)/2 + "px"; // 팝업 위치 정하고
+	popup.style.left = (window.innerWidth - popup.clientWidth)/2 + "px";
 
-	//뒷배경 멈춤
+	// //뒷배경 멈춤
+	const mainbody = document.querySelector(".wrap");
 	mainbody.style.position = "fixed"; 
 	mainbody.style.top = -scroll_top_holder + "px";
 
-	obj.querySelector(".closePopup").addEventListener("click", (e) => { closePopup(obj, mainbody); });
-
-	set_input(obj, mainbody);
+	popup.querySelector(".closePopup").addEventListener("click", closePopup);
 
 }
 
-function set_input(obj, mainbody){
+let timeCheck; 
+function set_input(popup){
 	//input 활성화
-	const guide = obj.querySelectorAll(".guide");
-	console.log(guide.length);
-	if( guide.length !== 0 )
-	{
-		for( let i=0; i<guide.length; i++ ) //가이드 개수만큼 input 있음, input에 리스너 달기
-		{
-			let parent = guide[i].parentNode;                
-			let tag = guide[i].nextElementSibling; 
-			let input = parent.querySelector(tag.nodeName);   //   }  
-			input.addEventListener("click", (e) => { enableInputFromGuide(e, obj); }, false ); 
+	let form =  popup.querySelector("form");
 
-		}
-
-		obj.querySelector("#agreeBox").addEventListener("click", (e) => { boxChecking(e, obj); });
-	}
-
-	//버튼 활성화
-
-	obj.querySelector(".submit").addEventListener("click", () => { 
-	    alert("준비중"); 
-	});
-
-	if( mainbody.className !== "subwrap")
-	    topsmallSlides(true);
-}
+	if( form === null )
+		return;
 
 
-function set_popup(noBg){
+	form.addEventListener('click', enableInputFromGuide);
+	popup.querySelector("#agreeBox").addEventListener("click", (e) => {
 
-	if( noBg )
-	{
-		console.log(`set_popup  background color`);
-		let popBg = document.createElement("div");
-		popBg.classList.add("popupBg");
-		document.body.appendChild(popBg);
-	}
-	else if( noBg === false && document.querySelector(".popupBg") !== null )
-	{
-		let popBg = document.querySelector(".popupBg");
-		let pp = popBg.parentNode;
-		pp.removeChild(popBg);
-	}
-    
-}
-
-
-function enableInputFromGuide(e, obj){ console.log("   enableInputFromGuide : ");
-
-	const nowTag = e.currentTarget;// input 태그 확인하고
-	const nowGuide = nowTag.previousElementSibling; //옆에 있는 가이드 태그 확인하고
-
-	const allguide = obj.querySelectorAll(nowGuide.nodeName);// guide 개수 확인
-
-	//입력폼들의 안내글을 모두 보이게, 입력폼에 내용이 있을 때는 안내글 안보이게
-	for( let i=0; i<allguide.length; i++ )
-	{            
-	    let input = allguide[i].nextElementSibling;
-
-	    if( input.value !== "" )
-	        allguide[i].classList.add("off");
-	    else
-	        allguide[i].classList.remove("off");            
-	}
-
-	nowGuide.classList.add("off");        
-	nowTag.focus();
-
-}
-
-
-function previewCheck(obj){ //, color
-
-	const allguide = obj.querySelectorAll(".guide");// guide 개수 확인
-
-	//입력폼 확인하기
-	for( let i=0; i<allguide.length; i++ )
-	{            
-		let input = allguide[i].nextElementSibling;
-
-		if( input.value === "" )
-		{
-			allguide[i].classList.remove("off");
-
-			let cn = allguide[i].className.split(" ");
-			for( let j=0; j<cn.length; j++)
-			{
-				if( cn[j] === "warning")
-					allguide[i].classList.remove("warning"); 
-				else
-					allguide[i].classList.add("warning");                    
-			}
-		}
-	//console.log("previewCheck " + i);
-	}
-}
-
-
-//동의 박스 체킹 여부 확인
-function boxChecking(e, obj){
-    
-	if( e.timeStamp !== timeCheck ) //두 번 실행되서 timeStamp 값을 확인한 후 한 번만 실행되게
-	{
+		if( e.timeStamp === timeCheck )
+			return;
 
 		let t = e.currentTarget;
 		let cn = t.className.split(" ");
-		let x=-1;
+		let isThere=false;
+
+		//console.log("111 ok ::: ", isThere);
 
 		for( let i=0; i<cn.length; i++)
 		{
-		    if( cn[i] === "on")
-		        x=i;
-		} 
+		    if( cn[i] === "ok")
+				isThere = true;
+		} //console.log("222 ok ::: ", isThere);
 
-		if( x >= 0)
-		    t.classList.remove("on");
+		if( isThere )
+		    t.classList.remove("ok");
 		else
-		    t.classList.add("on");
+		    t.classList.add("ok");
 
-		previewCheck(obj);
-
+		validateForm(popup);
 		timeCheck = e.timeStamp;
+
+	});
+
+	//버튼 활성화
+	//popup.querySelector(".submit").addEventListener("click", bmh);
+	topsmallSlides(true);
+
+}
+
+
+function set_popup(){
+
+	let bg = document.querySelector(".popupBg");
+
+	if( bg !== null )
+	{
+		document.body.removeChild(bg);
+		return;		
 	}
-    
+
+	let popBg = document.createElement("div");
+	popBg.classList.add("popupBg");
+	document.body.appendChild(popBg);
+
+}
+
+
+function enableInputFromGuide(e){ 
+
+	if( e.target.nodeName === this.nodeName )
+		return;
+
+	let nowTxtBox = e.target;
+	let nowGuide = nowTxtBox.previousElementSibling;
+
+	let txtBox = this.getElementsByClassName("txtBox");	
+
+	//입력폼에 내용이 있을 때는 안내글 안보이게
+	for( let i=0; i<txtBox.length; i++ )
+	{
+		let guide = txtBox[i].previousElementSibling;
+
+		if( txtBox[i].value === "" )
+			guide.classList.remove("off");
+		else
+			guide.classList.add("off");
+	}
+
+	nowGuide.classList.add("off");
+	nowTxtBox.focus();
+}
+
+
+function validateForm(popup){ console.log("validateForm ");
+
+	const txtBox = popup.getElementsByClassName("txtBox");	// guide 개수 확인
+
+	//입력폼 확인하기
+	for( let i=0; i<txtBox.length; i++ )
+	{          
+		let guide = txtBox[i].previousElementSibling; 
+
+		if( txtBox[i].value === "" )
+		{
+			guide.classList.remove("off");
+			guide.classList.add("warning");
+		}
+		else
+			guide.classList.remove("warning");
+	}
 }
 
 
 
 //팝업 닫기 버튼 설정
-function closePopup(obj, mainbody){   
+function closePopup(e){   
 
-	obj.style.display = "none";
+	let popup = e.currentTarget.parentNode;
+
+	popup.style.display = "none";
 
 	set_popup(false);
 
+	const mainbody = document.querySelector(".wrap");
 	mainbody.style.position = "relative";
 	let scroll_top_holder = mainbody.style.top.replace(/[^0-9]/g, "");
 
@@ -181,46 +166,19 @@ function closePopup(obj, mainbody){
 	    topsmallSlides(false);
 
 	// //입력폼 초기화
-	let input = obj.querySelectorAll("input");
-	for( let i=0; i<input.length; i++ )
-	    input[i].value = ""; 
-
-	let txtarea = obj.querySelectorAll("textarea");
-	for( let i=0; i<txtarea.length; i++ )
-	    txtarea[i].value = "";
-
-	let guide = obj.querySelectorAll(".guide");
-	for( let i=0; i<guide.length; i++ )
+	let txtBox = popup.getElementsByClassName("txtBox");
+	for( let i=0; i<txtBox.length; i++ )
 	{
-	    guide[i].classList.remove("off");
-	    guide[i].classList.remove("warning");
+	    txtBox[i].value = ""; 
+	    txtBox[i].previousElementSibling.classList.remove("off");
+	    txtBox[i].previousElementSibling.classList.remove("warning");
 	}
 
-	document.getElementById("agreeBox").classList.remove("on");
+	popup.querySelector("#agreeBox").classList.remove("ok");
+
+	popup.querySelector("#agreeBox").removeEventListener("click", () => {});
        
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 	
