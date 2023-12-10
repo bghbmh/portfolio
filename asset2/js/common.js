bd605ff88c8f604cd457e6de2f0a82645cc4e383
bghbmh <55042013+bghbmh@users.noreply.github.com>
2023-12-11 02:26:06 +0900

    경로수정

--- asset2/js/common.js
+++ asset2/js/common.js
@@ -7,7 +7,7 @@ import { Modal } from "./modalType2.js";
 document.addEventListener("DOMContentLoaded", () => {
 
 	console.log("DOMContentLoaded ")
-	fileHandler._load( { url: '../assets/data/bmh.json', callback : introHandler,loadType:"item", done: "items" });
+	fileHandler._load( { url: '../data/bmh.json', callback : introHandler,loadType:"item", done: "items" });
 
 	if( document.querySelector(".noteSticky") )
 		document.querySelector(".noteSticky").addEventListener("click", noteStickyHandler);
@@ -136,7 +136,7 @@ function cardListHandler(e){
 		case "detail":
 			
 			fileHandler._load( { 
-				url: '../assets/data/bmh.json', 
+				url: '../data/bmh.json', 
 				callback : function(request){
 					
 					let items = JSON.parse(request.responseText);
