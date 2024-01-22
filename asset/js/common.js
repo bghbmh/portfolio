010a495598155d04b6e1650b3f80c6c9f8039aef
bghbmh <55042013+bghbmh@users.noreply.github.com>
2024-01-22 16:12:33 +0000

    z

--- asset/js/common.js
+++ asset/js/common.js
@@ -7,7 +7,7 @@ import { Modal } from "./modalType3.js";
 document.addEventListener("DOMContentLoaded", () => {
 
 	console.log("DOMContentLoaded ")
-	fileHandler._load( { url: './data/bmh.json', callback : introHandler,loadType:"item", done: "items" });
+	fileHandler._load( { url: '../main/data/bmh.json', callback : introHandler,loadType:"item", done: "items" });
 
 
 	if( document.querySelector(".listTest") ){
