91e9e2512d5773edb360f6d6b09dad3bb1ae25af
bghbmh <55042013+bghbmh@users.noreply.github.com>
2024-02-11 14:09:38 +0900

    Update common.js

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,74 +1,12 @@
+import * as cf from './commonFunction.js';
+
+//import { fileHandler } from "./fileHandler.js";
 import * as component from './component.js';
-import { fileHandler } from "./fileHandler.js";
+import { SamplePageview } from './SamplePageview.js';
+
 import { Modal } from "./modalType3.js";
 // import { dataHandler } from "./dataHandler.js";
 
-
-class SamplePageview extends HTMLElement {
-	constructor(n = null, p = null, rt=null) {
-		// Always call super first in constructor
-		super();
-
-		this.sampleName = n;
-		this.samplePage = p;
-		this.rootPath = rt;
-	}
-
-	connectedCallback() {
-		// Create a shadow root
-		const shadow = this.attachShadow({ mode: "open" });
-		console.log("custom element - connectedCallback", shadow, this );
-
-		let rootPath = `../main/data/sample/` + ( this.sampleName || "temp1");
-
-		const linkElem = document.createElement("link");
-		linkElem.setAttribute("rel", "stylesheet");
-		linkElem.setAttribute("href", "../main/asset/css/samplepageView.css");
-
-
-		const linkElem2 = document.createElement("link");
-		linkElem2.setAttribute("rel", "stylesheet");
-		linkElem2.setAttribute("href", this.rootPath+ this.sampleName + "roothost.css");
-		
-		shadow.appendChild(linkElem);
-		shadow.appendChild(linkElem2);
-
-		const nav = null;
-		// if( this.samplePage.length ){
-		// 	nav = CreateElement({tag: "nav", class: "samplePageNav", "aria-label" : "샘플페이지 메뉴" });
-
-		// 	let html = '';
-		// 	this.samplePage.forEach( href => { html += `<a href="${href}">${href}</a>` });
-		// 	nav.innerHTML = html;	
-		// } else {
-
-		// }
-
-		//nav ? shadow.appendChild(nav) : '';
-
-		
-
-
-		// const btn = document.createElement("button");
-		// btn.setAttribute("type", "button");
-
-		const btn = CreateElement({tag: "button", class: "btn", type: "button", "aria-label" : "close", "data-ui-action":"close" });
-		btn.innerHTML = `<i class="fa-solid fa-clone"></i>`;
-		btn.addEventListener("click", e => {
-			console.log( "spv - ", this, e.target.closest("[data-ui-action]") );
-			if( !e.target.closest("[data-ui-action]") ) return; 
-
-			this.parentNode.classList.remove("openShadowDom");
-			this.parentNode.removeChild(this);
-		});
-		shadow.appendChild(btn);
-
-
-	}
-}
-
-// Define the new element
-customElements.define("sample-pageview", SamplePageview);
 
 
 
@@ -76,7 +14,7 @@ customElements.define("sample-pageview", SamplePageview);
 document.addEventListener("DOMContentLoaded", () => {
 
 	console.log("DOMContentLoaded ")
-	fileHandler._load( { url: '../main/data/bmh.json', callback : introHandler,loadType:"item", done: "items" });
+	cf.fileHandler._load( { url: '../main/data/bmh.json', callback : introHandler,loadType:"item", done: "items" });
 
 
 	if( document.querySelector(".listTest") ){
@@ -195,9 +133,9 @@ function introHandler(request) {
 
 	/* gnb 임시 */
 	if( !document.querySelector(".gnb") ){
-		let gnb = CreateElement({ tag : "NAV", class : "gnb", "data-target":"cardList"});
+		let gnb = cf.CreateElement({ tag : "NAV", class : "gnb", "data-target":"cardList"});
 		console.log(" click gnb");
-		let itemGroup = CreateElement({ tag : "UL", class : "tabType1"});
+		let itemGroup = cf.CreateElement({ tag : "UL", class : "tabType1"});
 
 		let category = [];
 		itemsData.forEach( item => {
@@ -240,7 +178,7 @@ function cardListHandler(e){
 			break;
 		case "detail":
 			
-			fileHandler._load( { 
+			cf.fileHandler._load( { 
 				url: '../main/data/bmh.json', 
 				callback : function(request){
 					
@@ -267,7 +205,7 @@ function cardListHandler(e){
 			
 			break;
 		case "preview":
-			console.log( " util test_preview - " );
+			console.log( " util test_preview - ");
 			document.querySelector("body").classList.toggle("openShadowDom");
 
 			//console.log("spv - ", clickElem.dataset.sampleName, JSON.parse(clickElem.dataset.samplePage));
@@ -278,96 +216,6 @@ function cardListHandler(e){
 			const spv = new SamplePageview(clickElem.dataset.sampleName, spArr, rootPath);
 			document.querySelector("body").appendChild(spv);
 
-			
-			
-			fileHandler._load( {  //  '../html/testMain.html'
-				url: rootPath + 'html/' + spArr[0], 
-				callback : function(request){
-
-					console.log("testpath - ", rootPath  );
-		
-					if( !request.arguments.done ){
-						console.log("fail - ", rootPath + clickElem.dataset.sampleName + '/' + spArr, request );
-						return;
-					}
-		
-					
-		
-					let head=``;
-					if(  request.response.includes("<head") ){
-						let start = -1;
-						let end = -1;
-						let pos = -1;
-						let tarStr = '<head';
-						while( (pos = request.response.indexOf(tarStr, pos + 1)) != -1 ) { 
-							//console.log(`현재 target의 위치는 [${pos}] 번째 입니다.`);
-							if( tarStr === '<head' ) tarStr = ">";
-							else if( tarStr === '>' ) break;
-							
-						}
-						start = pos+1;
-		
-						console.log(`xxxx [${pos}] `);
-		
-						tarStr = '</head>';
-						while( (pos = request.response.indexOf(tarStr, pos + 1)) != -1 ) { 
-							//console.log(`현재 target의 위치는 [${pos}] 번째 입니다.`);
-							end = pos;
-						}
-		
-						head = request.response.slice(start, end,);
-		
-						
-					}
-					
-					head = head.replaceAll('../', rootPath);
-		
-		
-		
-		
-					let html=``;
-					if(  request.response.includes("<body") ){
-						let start = -1;
-						let end = -1;
-						let pos = -1;
-						let tarStr = '<body';
-						while( (pos = request.response.indexOf(tarStr, pos + 1)) != -1 ) { 
-							//console.log(`현재 target의 위치는 [${pos}] 번째 입니다.`);
-							if( tarStr === '<body' ) tarStr = ">";
-							else if( tarStr === '>' ) break;
-							
-						}
-						start = pos+1;
-		
-						console.log(`xxxx [${pos}] `);
-		
-						tarStr = '</body>';
-						while( (pos = request.response.indexOf(tarStr, pos + 1)) != -1 ) { 
-							//console.log(`현재 target의 위치는 [${pos}] 번째 입니다.`);
-							end = pos;
-						}
-		
-						html = request.response.slice(start, end,);
-		
-						
-					}
-		
-					
-					html = html.replaceAll('../', rootPath);
-					
-		
-		
-					const shadowWrap = document.createElement("div");
-					shadowWrap.setAttribute("class", "shadowWrap");
-					shadowWrap.innerHTML = head + html;
-		
-					spv.shadowRoot.appendChild(shadowWrap);
-		
-		
-				},
-				loadType:"text/html", 
-				done: true 
-			});
 
 			break;
 		default:
@@ -376,16 +224,3 @@ function cardListHandler(e){
 	}
 }
 
-
-
-
-function CreateElement(attributes = {}) { // { tag : "div", class: "sample", ...} 
-	if (!attributes.hasOwnProperty("tag")) return alert("no Tag, require the Tag");
-
-	let tag = document.createElement(attributes.tag);
-	for (let prop in attributes) {
-		if (prop == "tag") continue;
-		tag.setAttribute(prop, attributes[prop]);
-	}
-	return tag;
-}
