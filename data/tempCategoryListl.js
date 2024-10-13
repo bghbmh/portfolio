05cb722bf61e3bbf419808d788b8ddfd00b80feb
bghbmh <55042013+bghbmh@users.noreply.github.com>
2024-10-14 01:46:21 +0900

    Add files via upload
    add

---
+++ data/tempCategoryListl.js
@@ -0,0 +1,24 @@
+
+export let ct = {
+	main : ["웹", "인쇄", "콘텐츠", "브랜딩", "react"],
+	sub :  ["제작", "구축","리뉴얼" ,"유지보수", "sample" ],
+	hash : ["디자인",
+		"마크업",
+		"모바일",
+		"데스크탑",
+		"반응형",
+		"마케팅",
+		"웹",
+		"콘텐츠",
+		"인쇄",
+		"브랜딩",
+		"ui-test"
+	]
+};
+
+export let tempDB = "myList.json"; // testList.json
+
+export let origin = location.origin; //https://bghbmh.github.io/main
+
+
+
