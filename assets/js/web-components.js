e705fcb96f34e333abd928c2c571e1eae427748a
bghbmh <55042013+bghbmh@users.noreply.github.com>
2025-07-07 01:28:19 +0900

    Update web-components.js
    a

--- assets/js/web-components.js
+++ assets/js/web-components.js
@@ -1,5 +1,7 @@
 
-import './portfolio/components/common-header.js';
-import '/components/common-footer.js';
-import '/components/card-type1.js';
-import '/components/modal-popup.js';
+console.log("components - ", location)
+
+import './components/common-header.js';
+import './components/common-footer.js';
+import './components/card-type1.js';
+import './components/modal-popup.js';
