9110ef8c3441004cbae0421766818a4fe8d890fe
bghbmh <55042013+bghbmh@users.noreply.github.com>
2025-07-07 01:15:39 +0900

    Delete data directory
    re

--- data/sample/hycu/mockup/test.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-
-
-    document.querySelector("title").textContent = document.querySelector('html').dataset.title ? document.querySelector('html').dataset.title  : "test";
-
-});
--- data/sample/keimyung/html/mockup/test.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log('same11 - ')
-
-    document.querySelector("title").textContent = document.querySelector('html').dataset.title ? document.querySelector('html').dataset.title  : "확인";
-
-
-});
